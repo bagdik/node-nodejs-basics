c02f6bf4d31a6e8b32a040286afa8444cdb2ae91
Bahdan Pryimachuk <bahdanchik@tut.by>
2022-06-04 19:46:19 +0300

    implement function copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,20 @@
+import fs from 'fs';
+import path from 'path'; 
+
 export const copy = async () => {
-    // Write your code here 
+  const src = 'src/fs/files';
+  const dest = 'src/fs/files_copy';
+  if (!fs.existsSync(src) || fs.existsSync(dest)) 
+    throw new Error('FS operation failed');
+  fs.mkdir(dest, err => {
+    if (err) throw err; 
+  })
+  fs.readdir(src, (err, files) => {
+    files.forEach(file => {
+      fs.copyFile(path.join(src, file), path.join(dest, file), err => {
+        if (err) throw err; 
+        console.log(`The file '${file}' has been copied!`);
+      });
+    });
+  });
 };
