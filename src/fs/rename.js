6a6cc154a93d02d837fcb0b23a5d8c86779163f0
Bahdan Pryimachuk <bahdanchik@tut.by>
2022-06-04 20:45:32 +0300

    add: implement function rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,12 @@
+import fs from 'fs';
+
 export const rename = async () => {
-    // Write your code here 
+  const wrongFalename = 'src/fs/files/wrongFilename.txt';
+  const properFilename = 'src/fs/files/properFilename.md';
+  if (!fs.existsSync(wrongFalename) || fs.existsSync(properFilename)) 
+    throw new Error('FS operation failed');
+  fs.rename(wrongFalename, properFilename, err => {
+    if (err) throw err;
+    console.log('The file has been renamed!');
+  });
 };
