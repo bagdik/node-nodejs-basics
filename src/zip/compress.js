a9d7fb797ada1f1bbee9649732f622432cd1871b
Bahdan Pryimachuk <bahdanchik@tut.by>
2022-06-05 15:43:02 +0300

    add: implement section zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,9 @@
+import zlib from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
 export const compress = async () => {
-    // Write your code here 
+  const gzip = zlib.createGzip();
+  const inp = createReadStream('src/zip/files/fileToCompress.txt');
+  const out = createWriteStream('src/zip/files/archive.gz');
+  inp.pipe(gzip).pipe(out);
 };
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,9 @@
+import zlib from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
 export const decompress = async () => {
-    // Write your code here 
+  const gunzip = zlib.createGunzip();
+  const inp = createReadStream('src/zip/files/archive.gz');
+  const out = createWriteStream('src/zip/files/fileToCompress.txt');
+  inp.pipe(gunzip).pipe(out);
 };
