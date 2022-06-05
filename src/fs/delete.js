67bf838bab387da28251afd6780c7c94e272e42d
Bahdan Pryimachuk <bahdanchik@tut.by>
2022-06-05 08:13:30 +0300

    add: implement function delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,10 @@
+import { unlink, existsSync } from 'fs';
+
 export const remove = async () => {
-    // Write your code here 
+    const fileToRemove = 'src/fs/files/fileToRemove.txt';
+    if (!existsSync(fileToRemove)) throw new Error('FS operation failed');
+    unlink(fileToRemove, err => {
+      if (err) throw err;
+      console.log('The file has been deleted!');
+    });
 };
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
 
 export const rename = async () => {
-  const wrongFalename = 'src/fs/files/wrongFilename.txt';
+  const wrongFilename = 'src/fs/files/wrongFilename.txt';
   const properFilename = 'src/fs/files/properFilename.md';
-  if (!fs.existsSync(wrongFalename) || fs.existsSync(properFilename)) 
+  if (!fs.existsSync(wrongFilename) || fs.existsSync(properFilename)) 
     throw new Error('FS operation failed');
   fs.rename(wrongFalename, properFilename, err => {
     if (err) throw err;
