c3f15aa514df5446db68d982cffd96a1130e1292
Bahdan Pryimachuk <bahdanchik@tut.by>
2022-06-05 09:11:26 +0300

    add: implement function read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,10 @@
+import { readFile, existsSync } from 'fs';
+
 export const read = async () => {
-    // Write your code here 
+  const fileToRead = 'src/fs/files/fileToRead.txt';
+  if(!fileToRead) throw new Error('FS operation failed');
+  readFile(fileToRead, 'utf-8', (err, data) => {
+    if(err) throw err;
+    console.log(data);
+  });
 };
