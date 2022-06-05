36a028468d08cc75d14d1b980caa265b4c322b00
Bahdan Pryimachuk <bahdanchik@tut.by>
2022-06-05 13:10:54 +0300

    add: imlement function calculateHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,9 @@
+import crypto from 'crypto';
+import { readFileSync } from 'fs';
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const file = './src/hash/files/fileToCalculateHashFor.txt';
+  const data = readFileSync(file, 'utf-8');
+  const hash = crypto.createHash('sha256');
+  return hash.update(data).digest('hex');
 };
